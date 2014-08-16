63d9b2a7681dd2fbd045cf1e5ec60935c525dae6
J.S.Sebastian <j.s.sebastian@yandex.com>
2014-08-16 14:23:38 +0200

    Change buffer_rx

--- serial.h
+++ serial.h
@@ -55,4 +55,11 @@ uint8_t serial_read();
 // Reset and empty data in read buffer. Used by e-stop and reset.
 void serial_reset_read_buffer();
 
+// Returns the number of bytes used in the RX serial buffer.
+uint8_t serial_get_rx_buffer_count();
+
+// Returns the number of bytes used in the TX serial buffer.
+// NOTE: Not used except for debugging and ensuring no TX bottlenecks.
+uint8_t serial_get_tx_buffer_count();
+
 #endif
